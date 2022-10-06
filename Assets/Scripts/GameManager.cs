6c60152f0ace9cf2062f5d90659b682b4e264f34
Alex99-bit <alexco_99@hotmail.com>
2022-10-05 23:34:21 -0500

    Update the game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,39 +19,53 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Some controllers to the game manager, like when the player press pause or some like that
+        /* if player press (pause button) then game pauses and call SetGameState(GameState.pause) : is how it works */
+
     }
 
-    void SetGameState(GameState state)
+    void SetGameState(GameState newState)
     {
-        switch (state)
+        // It switch between the states
+        switch (newState)
         {
-            case GameState.start:
-
+            case GameState.menu:
+                Time.timeScale = 0;
                 break;
+
+            case GameState.inGame:
+                Time.timeScale = 1;
+                break;
+
             case GameState.pause:
-                
+                Time.timeScale = 0;
                 break;
-            case GameState.gameover:
 
+            case GameState.gameover:
+                Time.timeScale = 0.3f;
                 break;
+
             default:
                 Debug.Log("There's no valid state or they are not working");
                 break;
         }
+
+        // here update the current game state to the new state... easy...
+        currentGameState = newState;
     }
 }
 
 // in this enum are the many game states like start, pause and game over...
 public enum GameState
 {
-    start,
+    menu,
+    inGame,
     pause,
-    gameover,
+    gameover
 }
