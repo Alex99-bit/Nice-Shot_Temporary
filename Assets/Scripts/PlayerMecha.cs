79180903c65773942a1419e7baac4b84e41d7d74
Carlos Alejandro <alexco_99@hotmail.com>
2022-12-02 16:34:04 -0600

    cosas

--- Assets/Scripts/PlayerMecha.cs
+++ Assets/Scripts/PlayerMecha.cs
@@ -15,6 +15,9 @@ public class PlayerMecha : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameManager.sharedInstance.currentGameState == GameStates.inGame)
+        {
+
+        }   
     }
 }
