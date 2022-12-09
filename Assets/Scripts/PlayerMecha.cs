fbef11577ea20c2601e235301a486dbafeb6cd3b
Alejandro Coronado <alexco_99@hotmail.com>
2022-12-09 15:59:48 -0600

    more cambios

--- Assets/Scripts/PlayerMecha.cs
+++ Assets/Scripts/PlayerMecha.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class PlayerMecha : MonoBehaviour
 {
     Rigidbody2D playerRB;
+    AnimaPlayer playerAnim;
+    public float speed;
+    public int life;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+
+        if(speed == 0)
+        {
+            speed = 5.5f;
+        }
+
+        if(life == 0)
+        {
+            life = 3;
+        }
+
+        playerAnim = AnimaPlayer.idle;
     }
 
     // Update is called once per frame
@@ -17,7 +32,78 @@ public class PlayerMecha : MonoBehaviour
     {
         if(GameManager.sharedInstance.currentGameState == GameStates.inGame)
         {
-
+            Movement();
+            Apunta();
+            Dispara();
         }   
     }
+
+    void Movement()
+    {
+        // Variables locales que solo funcionan dentro de este metodo, para administrar el movimiento
+        float moveX, moveY;
+
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        // Movimiento del jugador y sus animaciones
+        playerRB.velocity = new Vector2(moveX, moveY) * speed;
+
+        // Animaciones respecto al movimiento en X u Horizontalmente
+        if (moveX > 0)
+        {
+            // Va hacia la derecha
+            playerAnim = AnimaPlayer.run;
+        }
+        else if (moveX < 0)
+        {
+            // Va hacia la izquierda
+            playerAnim = AnimaPlayer.run;
+        }
+        else if(moveX == 0)
+        {
+            // Se queda en el idle
+            playerAnim = AnimaPlayer.idle;
+        }
+
+        // Animaciones respecto al movimiento en Y o Verticalmente
+        if (moveY > 0)
+        {
+            // Se mueve hacia arriba
+            playerAnim = AnimaPlayer.run;
+
+        }
+        else if (moveY < 0)
+        {
+            // Se mueve hacia abajo
+            playerAnim = AnimaPlayer.run;
+
+        }
+        else if (moveY == 0)
+        {
+            // Se queda en idle
+            playerAnim = AnimaPlayer.idle;
+
+        }
+    }
+
+    void Apunta()
+    {
+        // Apunta hacia donde este el cursor del mouse
+
+    }
+
+    void Dispara()
+    {
+        // Dispara y crea la animacion
+
+    }
+}
+
+public enum AnimaPlayer
+{
+    idle,
+    run,
+    fire,
+    dead
 }
