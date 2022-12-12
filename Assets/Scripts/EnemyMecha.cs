915970bf6154b0faebf10eb7f09f3bf8b249228d
Carlos Alejandro <alexco_99@hotmail.com>
2022-12-12 17:45:57 -0600

    seresrs

--- Assets/Scripts/EnemyMecha.cs
+++ Assets/Scripts/EnemyMecha.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class EnemyMecha : MonoBehaviour
 {
+    Rigidbody2D enemyRB;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
